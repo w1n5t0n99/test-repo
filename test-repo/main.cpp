516fb967bb4f83af0a6d7a8da98b6d4b9b029670
relam <relam@rcschools.us>
2018-04-16 14:50:50 -0400

    add new print function

--- test-repo/main.cpp
+++ test-repo/main.cpp
@@ -5,10 +5,16 @@ void test_print()
 	std::cout << "print wip" << std::endl;
 }
 
+void test_print2()
+{
+	std::cout << "test print 2" << std::endl;
+}
+
 int main()
 {
 
 	test_print();
+	test_print2();
 	std::cin.get();
 	return 0;
 }
